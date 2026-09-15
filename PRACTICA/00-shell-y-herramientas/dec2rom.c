4aab71821fa7e529548543d51566cdca8bc967cf
Asier Marzo <amarzo@hotmail.com>
2026-09-15 21:04:35 +0200

    dec2rom constante el número de símbolos en vez hardcoded

--- PRACTICA/00-shell-y-herramientas/dec2rom.c
+++ PRACTICA/00-shell-y-herramientas/dec2rom.c
@@ -3,10 +3,10 @@
 int main(void) {
     const int valores[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     const char *simbolos[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-    int n;
+    const int num_simbolos = sizeof(valores) / sizeof(valores[0]);
 
-    while (scanf("%d", &n) == 1) {
-        for (int i = 0; i < 13; i++)
+    for (int n; scanf("%d", &n) == 1; ) {
+        for (int i = 0; i < num_simbolos; i++)
             while (n >= valores[i]) {
                 printf("%s", simbolos[i]);
                 n -= valores[i];
